67381c9b6f006b9d66a316682c7f359d33a2cb88
dokajajob <dokajajob@gmail.com>
2021-03-11 12:20:45 +0200

    nnn

--- notes.js
+++ notes.js
@@ -32,8 +32,7 @@ afterEach(function(){
 
     element(by.id('gobutton')).click();
 
-    expect(element(by.binding('latest')).getText()).
-        toEqual('3'); // This is wrong!
+    expect(element(by.binding('latest')).getText()).toEqual('3'); // This is wrong!
   });
 
 
@@ -79,4 +78,75 @@ it('should be on correct page', function() {
   	});
 
 
+-------------------------------------------------
 
+https://stackoverflow.com/questions/31744526/protractor-checking-if-input-field-has-text
+
+-------------------------------------------------
+
+expect(hasClass(element(by.name('your-element')), 'ng-invalid')).toBe(true);
+expect(hasClass(element(by.name('your-element')), 'ng-dirty')).toBe(true);
+
+------------------------------------------------
+
+https://www.seleniumeasy.com/protractor-angularjs-tutorials/ways-to-check-if-element-is-present-example
+
+------------------------------------------------
+
+https://namitamalik.github.io/E2E-testing-with-Protractor/
+
+
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec.js'],
+    multiCapabilities: [{
+        browserName: 'firefox'
+    }, {
+        browserName: 'chrome'
+    }]
+};
+
+
+**** can be used as complete test ***
+
+
+(function () {
+    function openApplicationInBrowser() {
+        browser.get("http://localhost:8080/creditCard.html");
+    }
+    describe('Saving Credit Card Number', function () {
+        beforeEach(function () {
+            openApplicationInBrowser();
+        });
+        it('should have correct title', function () {
+            expect(browser.getTitle()).toEqual('Credit Card');
+        });
+        it('checks if the input field is by default disabled', function () {
+            expect(element(by.model('data.cardNumber')).isEnabled()).toBe(false);   *** check if can be used with button ***
+            expect(element(by.id('save')).isEnabled()).toBe(false);
+        });
+        it('enables the input field', function () {
+            element(by.model('data.checkCard')).click();
+            expect(element(by.model('data.cardNumber')).isEnabled()).toBe(true);
+        });
+        it('gives an error message on writing invalid credit card number', function () {   *** test this ***
+            element(by.model('data.checkCard')).click();
+            element(by.model('data.cardNumber')).sendKeys("abcdefghijkikiki");
+            element(by.id('save')).click();
+            expect(element(by.binding('errorMessage')).getText()).toEqual("Credit card number can have only Numbers(0-9)");
+        });
+        it('gives a success message on writing a valid credit card number', function () {    *** test this ***
+            var cardNumber = "1234567899009876";
+            element(by.model('data.checkCard')).click();
+            element(by.model('data.cardNumber')).sendKeys(cardNumber);
+            element(by.id('save')).click();
+            expect(element(by.binding('successMessage')).getText()).toEqual("Your credit card number " + cardNumber + " has been saved with us.");
+        });
+        it('gives an error message when credit card number entered is less than 16 digits', function () {   *** test this ***
+            element(by.model('data.checkCard')).click();
+            element(by.model('data.cardNumber')).sendKeys("1234567890");
+            element(by.id('save')).click();
+            expect(element(by.binding('errorMessage')).getText()).toEqual("Please enter a valid credit card number");
+        });
+    });
+})();
